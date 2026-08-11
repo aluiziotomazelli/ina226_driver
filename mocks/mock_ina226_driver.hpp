6fa63faa157bb817dc0a519e72ee99be42644ddf
Aluizio Tomazelli <aluiziotomazelli@gmail.com>
2026-08-11 01:19:28 -0400

    fix: mock_ina226_driver for consumers

--- mocks/mock_ina226_driver.hpp
+++ mocks/mock_ina226_driver.hpp
@@ -8,12 +8,14 @@ namespace ina226 {
 class MockIna226Driver : public IIna226Driver
 {
 public:
+    MOCK_METHOD(esp_err_t, init, (i2c_master_bus_handle_t bus_handle), (override));
     MOCK_METHOD(esp_err_t, init, (), (override));
     MOCK_METHOD(esp_err_t, reset, (), (override));
     MOCK_METHOD(esp_err_t, read_shunt_voltage_uv, (int32_t& out_uv), (override));
     MOCK_METHOD(esp_err_t, read_bus_voltage_mv, (uint16_t& out_mv), (override));
     MOCK_METHOD(esp_err_t, read_current_ma, (float& out_ma), (override));
     MOCK_METHOD(esp_err_t, read_power_mw, (float& out_mw), (override));
+    MOCK_METHOD(esp_err_t, set_config, (const Ina226Config& new_config), (override));
     MOCK_METHOD(esp_err_t, calibrate, (float r_shunt_ohms, float max_expected_current_a), (override));
     MOCK_METHOD(esp_err_t, configure_alert, (uint16_t alert_mask, uint16_t alert_limit), (override));
     MOCK_METHOD(esp_err_t, read_alert_flags, (uint16_t& out_flags), (override));
